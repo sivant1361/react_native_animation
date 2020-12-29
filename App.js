95e17db495a3eb9323629bac80de8b69b0c3df96
sivant1361 <55569255+sivant1361@users.noreply.github.com>
2020-12-29 23:26:34 +0530

    my first animation

--- App.js
+++ App.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Animated, TouchableOpacity, Text} from 'react-native';
 
 export default function App() {
-  function moveBall() {}
+  const value = useState(new Animated.ValueXY({x: 0, y: 0}))[0];
+
+  function moveBall() {
+    Animated.timing(value, {
+      toValue: {x: 100, y: 100},
+      duration: 1500,
+      useNativeDriver: false,
+    }).start();
+  }
 
   return (
     <View>
-      <Animated.View style={{borderRadius:200,backgroundColor:'red',height:100,width:100}}>
-
+      <Animated.View style={value.getLayout()}>
+        <View
+          style={{
+            borderRadius: 200,
+            backgroundColor: 'red',
+            height: 100,
+            width: 100,
+          }}></View>
       </Animated.View>
 
-      <TouchableOpacity>
+      <TouchableOpacity onPress={moveBall}>
         <Text>Click me</Text>
       </TouchableOpacity>
     </View>
